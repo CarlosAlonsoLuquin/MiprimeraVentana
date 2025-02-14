e2509addd4c52291041f6722afd293af2be9b5f5
CarlosAlonsoLuquin <carlos.luquinlopez2000@gmail.com>
2025-02-13 18:57:29 -0700

    Add files via upload

---
+++ src/Idsrv/Ventana.java
@@ -0,0 +1,20 @@
+package Idsrv;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+
+	public Ventana() {
+		this.setTitle("APOCOSI");
+		this.setVisible(true);
+		this.setSize(500,500);
+		
+		this.setLocationRelativeTo(null);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setResizable(true);
+		
+	}
+
+}
---
+++ src/Idsrv/main.java
@@ -0,0 +1,16 @@
+package Idsrv;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		Ventana Ventana= new Ventana();
+		
+		
+
+	}
+
+}
