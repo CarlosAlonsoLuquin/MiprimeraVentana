ae407f0bf3709589c2955d4849e403da97e0c34e
CarlosAlonsoLuquin <carlos.luquinlopez2000@gmail.com>
2025-02-15 21:53:49 -0700

    LOGIN
    trabajo 9

---
+++ src/IDSTV/main.java
@@ -0,0 +1,12 @@
+package IDSTV;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		ventana Ventana=new ventana();
+		
+
+	}
+
+}
---
+++ src/IDSTV/ventana.java
@@ -0,0 +1,20 @@
+package IDSTV;
+
+import javax.swing.JFrame;
+
+public class ventana extends JFrame {
+
+	public ventana() {
+		this.setTitle("Hola");
+		this.setVisible(true);
+		this.setSize(500,500);
+		
+		this.setLocationRelativeTo(null);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setResizable(true);
+		
+	}
+
+}
